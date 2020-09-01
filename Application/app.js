a9076cb0c8096f5f9f5030269325df584c4fe547
Joshua Sandberg <JoshuaSandberg@Joshuas-MacBook-Pro.local>
2020-08-31 22:16:35 -0700

    fixxed delete function

--- Application/app.js
+++ Application/app.js
@@ -8,66 +8,60 @@ const path = require("path");
 const app = express();
 const PORT = 80;
 
-const notes = require("./db/db.json") || []
+let notes = require("./db/db.json")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"))
 
-const dbNotes = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
-      if (err) throw err;
-  })
-  );
+// const notes = JSON.parse(
+//   fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
+//       if (err) throw err;
+//   })
+//   );
   
-  const dbUpdate = dbNotes => {
-  fs.writeFileSync(
+  const dbUpdate = (data,res) => {
+  fs.writeFile(
       path.join(__dirname, "/db/db.json"),
-      JSON.stringify(dbNotes),
+      JSON.stringify(data),
       err => {
       if (err) throw err;
+      res.json("ok")
       }
   );
   };
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-
-});
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 
-
 });
 
 
+
 app.get("/api/notes", function(req, res) {
-    return res.json(dbNotes);
+    res.json(notes);
   });
   
 
 
   app.post("/api/notes", function(req, res) {
     let newNote = req.body;
-    let id = dbNotes.length;
+    let id = notes.length;
     newNote.id = id + 1;
-    dbNotes.push(newNote);
-    dbUpdate(dbNotes);
-    return res.json(dbNotes);
+    notes.push(newNote);
+    dbUpdate(notes, res);
     });
 
 
     app.delete("/api/notes/:id", (req, res) => {
       let id = req.params.id;
-      delete dbNotes[id - 1];
-      var filter = dbNotes.filter(function (el) {
-        return el != null;
-      });
-      dbUpdate(filter);
-      res.send(dbNotes);
+      console.log(id)
+      notes = notes.filter(note=> note.id != id);
+      dbUpdate(notes, res);
       });
 
+app.get("*", (req,res)=> res.sendFile(path.join(__dirname, "./public/index.html")));
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
