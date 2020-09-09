7453577958dfdd4254b2d348c938306bec8611d1
Your Name <you@example.com>
2020-09-08 18:16:39 -0700

    hello

--- Application/app.js
+++ Application/app.js
@@ -31,6 +31,10 @@ app.use(express.static("public"))
   );
   };
 
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+
+});
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
